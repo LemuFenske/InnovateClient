c808d7216b0f1f8f2e04ffeacfe12d8a97335ce1
Lemuel Fenske <lemueljosias.03@gmail.com>
2024-04-23 16:45:48 -0300

    portfolio5

--- client/src/components/SectionInstagram/SectionInstagram.jsx
+++ client/src/components/SectionInstagram/SectionInstagram.jsx
@@ -8,7 +8,7 @@ export default function SectionInstagram() {
     <section id="nuestros-productos" className="bg-white text-black p-8 min-h-screen w-full">
       <div className="flex flex-wrap justify-center items-center gap-4 md:gap-8">
         {/* Primera imagen */}
-        <div className="w-full md:w-1/3 overflow-hidden border-2 border-black rounded-md transform transition-transform hover:scale-105">
+        <div className="w-full md:w-3/8 overflow-hidden border-2 border-black rounded-md transform transition-transform hover:scale-105">
           <Link href="https://aprenderemprendedores.com/" target="_blank">
             <div>
               <Image
@@ -20,7 +20,7 @@ export default function SectionInstagram() {
           </Link>
         </div>
         {/* Segunda imagen */}
-        <div className="w-full md:w-1/3 overflow-hidden border-2 border-black rounded-md transform transition-transform hover:scale-105">
+        <div className="w-full md:w-3/8 overflow-hidden border-2 border-black rounded-md transform transition-transform hover:scale-105">
           <Link href="https://maimaramates.com/" target="_blank">
             <div>
               <Image
